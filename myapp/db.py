161b1ab7c8f1a200b55489c769026fe4ad08d21b
HKhawaja1 <>
2023-12-20 21:28:36 +0000

    Deploying The Application

--- myapp/db.py
+++ myapp/db.py
@@ -1,87 +1,57 @@
-import os
 import psycopg2
 
-# Load Environment Variables From .env File
-from dotenv import load_dotenv
-load_dotenv()
-
-# Details For Connecting To PostgreSQL Database
 db_config = {
-    "database": os.environ.get("HEROKU_DATABASE_NAME"),
-    "user": os.environ.get("HEROKU_DATABASE_USER"),
-    "password": os.environ.get("HEROKU_DATABASE_PASSWORD"),
-    "host": os.environ.get("HEROKU_DATABASE_HOST"),
-    "port": os.environ.get("HEROKU_DATABASE_PORT")
+    "database": "d505v2kg3k3eq7",
+    "user": "otdttvuukmgbtg",
+    "password": "be281bccd70b488ea82b35067ccde3e3e5d030c088bdace1220c6d7269154c57",
+    "host": "ec2-34-242-154-118.eu-west-1.compute.amazonaws.com",
+    "port": "5432"
 }
 
 
-# Connects To Database Using Provided Details
 def get_db_connection():
     return psycopg2.connect(**db_config)
 
 
-# Checks If Bookings Already Exists
-def db_check_booking(
-        new_first_name,
-        new_last_name,
-        new_num_people,
-        new_date,
-        new_time,
-        username):
+def db_check_booking(new_first_name, new_last_name, new_num_people, new_date, new_time, username):
     try:
-        conn = get_db_connection()  # Starts New Database Connection
+        conn = get_db_connection()
         cursor = conn.cursor()
         query = """
-            SELECT * FROM bookings
-            WHERE first_name=%s AND
-            last_name=%s AND
-            num_people=%s AND
-            date = %s AND
-            time = %s AND
-            userd = %s;
-                """
+            SELECT * FROM bookings WHERE first_name=%s AND last_name=%s AND num_people=%s AND date = %s AND time = %s AND userd = %s;
+            """
         cursor.execute(query, (new_first_name, new_last_name,
                        new_num_people, new_date, new_time, username))
         options = cursor.fetchall()
-        conn.close()  # Closes Database Connection
+        conn.close()
         return options
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
 
-# Inserts New Booking Into Database
-def db_insert_new_booking(
-        new_first_name,
-        new_last_name,
-        new_num_people,
-        new_date,
-        new_time,
-        username):
+def db_insert_new_booking(new_first_name, new_last_name, new_num_people, new_date, new_time, username):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         query = """
-            INSERT INTO bookings
-            (first_name, last_name, num_people, date, time, userd)
-            VALUES (%s, %s, %s, %s, %s, %s);
-                """
+            INSERT INTO bookings (first_name, last_name, num_people, date, time,userd) VALUES (%s, %s, %s, %s, %s,%s);
+            """
         cursor.execute(query, (new_first_name, new_last_name,
                        new_num_people, new_date, new_time, username))
-        conn.commit()  # Commits Transaction
+        conn.commit()  # Commit the transaction after an INSERT
         conn.close()
         return "Booking successfully inserted."
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
 
-# Retrieves Bookings From User
 def db_get_all_bookings(username):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         query = """
             SELECT * FROM bookings where userd = %s;
-                """
+            """
         cursor.execute(query, (username,))
         options = cursor.fetchall()
         conn.close()
@@ -90,14 +60,13 @@ def db_get_all_bookings(username):
         return f"An error occurred: {str(e)}"
 
 
-# Retrieves Booking From ID
 def db_get_booking(id):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         query = """
             SELECT * FROM bookings where id=%s;
-                """
+            """
         cursor.execute(query, (id,))
         options = cursor.fetchall()
         conn.close()
@@ -106,14 +75,13 @@ def db_get_booking(id):
         return f"An error occurred: {str(e)}"
 
 
-# Deletes Booking From Database By Its ID
 def db_delete_booking(id):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         query = """
             DELETE FROM bookings WHERE id=%s;
-                """
+            """
         cursor.execute(query, (id,))
         conn.commit()
         conn.close()
@@ -122,14 +90,7 @@ def db_delete_booking(id):
         return f"An error occurred: {str(e)}"
 
 
-# Updates Existing Booking In Database
-def db_update_booking(
-        id,
-        new_first_name,
-        new_last_name,
-        new_num_people,
-        new_date,
-        new_time):
+def db_update_booking(id, new_first_name, new_last_name, new_num_people, new_date, new_time):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -137,7 +98,7 @@ def db_update_booking(
             UPDATE bookings
             SET first_name=%s, last_name=%s, num_people=%s, date=%s, time=%s
             WHERE id=%s;
-            """
+        """
         cursor.execute(query, (new_first_name, new_last_name,
                        new_num_people, new_date, new_time, id))
         conn.commit()
